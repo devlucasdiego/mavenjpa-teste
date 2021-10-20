5b1c24f649d9ab8232c6a25511cbec7271db3cdc
Lucas Diego <luucasdiego_12@hotmail.com>
2021-10-20 17:15:27 -0400

    Projeto criado.

---
+++ src/main/java/aplicativo/Programa.java
@@ -0,0 +1,12 @@
+package aplicativo;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa(1, "Lucas Diego", "exemplo@hotmail.com");
+		System.out.println(p1);
+	}
+
+}
