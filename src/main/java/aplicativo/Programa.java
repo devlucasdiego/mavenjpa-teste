763b6204c54368c33997c80b9f7245f407fa3b44
Lucas Diego <luucasdiego_12@hotmail.com>
2021-10-20 22:15:51 -0400

    Configuração do persistence.xml e pom.xml

--- src/main/java/aplicativo/Programa.java
+++ src/main/java/aplicativo/Programa.java
@@ -1,12 +1,21 @@
 package aplicativo;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(1, "Lucas Diego", "exemplo@hotmail.com");
-		System.out.println(p1);
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 
 }
